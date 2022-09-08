f8c95c45853fcf2e0a18fc3bd9d584b21c58f408
capotebr1 <105760167+capotebr1@users.noreply.github.com>
2022-09-08 00:12:05 -0300

    Update abb.h

--- include/abb.h
+++ include/abb.h
@@ -21,18 +21,22 @@ TAbb darBaja(unsigned int ci , TAbb abb);
 //Devuelve la informacion del nodo "empleado" de abb
 TInfo raiz(TAbb abb);
 
-
-
 //Devuelve el subarbol que tiene el nodo cuyo componente CI coincide con "cedulaIdentidad".
 //Si "cedulaIdentidad" no pertenece a "abb", devuelve el árbol vacío.
 TAbb buscarSubarbol(unsigned int cedulaIdentidad, TAbb abb);
 
+//Imprime la información del empleado cuya cedula sea "cedula"
 void infoEmpleado(unsigned int cedula , TAbb abb);
 
+//Imprime la informacion de todos los empleados de la empresa.
 void imprimirEmpleados(TAbb abb);
 
+//Devuelve la información del menor nodo (cedula mas chica) del subarbol "abb"
 TInfo menorEnAbb(TAbb abb);
 
+//Devuelve la información del mayor nodo (cedula mas chica) del subarbol "abb"
 TInfo mayorEnAbb(TAbb abb);
 
+//Modifica la información del empleado cuya cedula coincide con "cedulaIdentidad"
+//Sin modificar la cedula
 TAbb modificarEmpleado(unsigned int cedulaIdentidad , TAbb abb);
